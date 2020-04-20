21361c9a95ab7adf4ce2f10e9d6e8f0c4067bcd1
kirito <kirito61254@gmail.com>
2020-04-20 17:53:14 +0900

    :sparkles: u8, u32 타입 추가

--- index.ts
+++ index.ts
@@ -1,3 +1,19 @@
 export const str = (value: string): object => {
   return { '@__type': 'str', '#text': String(value) };
 }
+
+export const u8 = (value: number | Array<number>): object => {
+  if (Array.isArray(value)) {
+    return { '@__type': 'u8', '@__count': value.length, '#text': value.join(' ') };
+  } else {
+    return { '@__type': 'u8', '#text': Number(value) };
+  }
+}
+
+export const u32 = (value: number | Array<number>): object => {
+  if (Array.isArray(value)) {
+    return { '@__type': 'u32', '@__count': value.length, '#text': value.join(' ') };
+  } else {
+    return { '@__type': 'u32', '#text': Number(value) };
+  }
+}
