8043a5b779dd01f289265ec6f39dbeb35232e825
kirito <kirito61254@gmail.com>
2020-04-21 06:26:20 +0900

    :sparkles: float, double, binary, time, ip4, s8, s16, s64, u16, u64 타입 추가 :art: 타입 마다 기본값 추가

--- index.ts
+++ index.ts
@@ -1,8 +1,44 @@
-export const str = (value: string): object => {
+export const str = (value: string = ''): object => {
   return { '@__type': 'str', '#text': String(value) };
 }
 
-export const s32 = (value: number | Array<number>): object => {
+export const float = (value: number = 0.0): object => {
+  return { '@__type': 'float', '#text': value };
+}
+
+export const double = (value: number = 0.0): object => {
+  return { '@__type': 'double', '#text': value };
+}
+
+export const bin = (value: string = ''): object => {
+  return { '@__type': 'str', '@__size': value.length, '#text': value };
+}
+
+export const time = (value: Date = new Date()): object => {
+  return { '@__type': 'time', '#text': value.getTime() };
+}
+
+export const ip4 = (value: string = '0.0.0.0'): object => {
+  return { '@__type': 'ip4', '#text': value };
+}
+
+export const s8 = (value: number | Array<number> = 0): object => {
+  if (Array.isArray(value)) {
+    return { '@__type': 's8', '@__count': value.length, '#text': value.join(' ') };
+  } else {
+    return { '@__type': 's8', '#text': Number(value) };
+  }
+}
+
+export const s16 = (value: number | Array<number> = 0): object => {
+  if (Array.isArray(value)) {
+    return { '@__type': 's16', '@__count': value.length, '#text': value.join(' ') };
+  } else {
+    return { '@__type': 's16', '#text': Number(value) };
+  }
+}
+
+export const s32 = (value: number | Array<number> = 0): object => {
   if (Array.isArray(value)) {
     return { '@__type': 's32', '@__count': value.length, '#text': value.join(' ') };
   } else {
@@ -10,7 +46,15 @@ export const s32 = (value: number | Array<number>): object => {
   }
 }
 
-export const u8 = (value: number | Array<number>): object => {
+export const s64 = (value: number | Array<number> = 0): object => {
+  if (Array.isArray(value)) {
+    return { '@__type': 's64', '@__count': value.length, '#text': value.join(' ') };
+  } else {
+    return { '@__type': 's64', '#text': Number(value) };
+  }
+}
+
+export const u8 = (value: number | Array<number> = 0): object => {
   if (Array.isArray(value)) {
     return { '@__type': 'u8', '@__count': value.length, '#text': value.join(' ') };
   } else {
@@ -18,10 +62,26 @@ export const u8 = (value: number | Array<number>): object => {
   }
 }
 
-export const u32 = (value: number | Array<number>): object => {
+export const u16 = (value: number | Array<number> = 0): object => {
+  if (Array.isArray(value)) {
+    return { '@__type': 'u16', '@__count': value.length, '#text': value.join(' ') };
+  } else {
+    return { '@__type': 'u16', '#text': Number(value) };
+  }
+}
+
+export const u32 = (value: number | Array<number> = 0): object => {
   if (Array.isArray(value)) {
     return { '@__type': 'u32', '@__count': value.length, '#text': value.join(' ') };
   } else {
     return { '@__type': 'u32', '#text': Number(value) };
+  }
+}
+
+export const u64 = (value: number | Array<number> = 0): object => {
+  if (Array.isArray(value)) {
+    return { '@__type': 'u64', '@__count': value.length, '#text': value.join(' ') };
+  } else {
+    return { '@__type': 'u64', '#text': Number(value) };
   }
 }
