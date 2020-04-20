6e7147b64df9e72f5a7ad29cf1bcb65fc8ac545a
kirito <kirito61254@gmail.com>
2020-04-20 17:53:24 +0900

    :sparkles: u8, u32 타입 추가

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,4 @@
 export declare const str: (value: string) => object;
+export declare const u8: (value: number | number[]) => object;
+export declare const u32: (value: number | number[]) => object;
 //# sourceMappingURL=index.d.ts.map
--- index.js
+++ index.js
@@ -3,3 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.str = function (value) {
     return { '@__type': 'str', '#text': String(value) };
 };
+exports.u8 = function (value) {
+    if (Array.isArray(value)) {
+        return { '@__type': 'u8', '@__count': value.length, '#text': value.join(' ') };
+    }
+    else {
+        return { '@__type': 'u8', '#text': Number(value) };
+    }
+};
+exports.u32 = function (value) {
+    if (Array.isArray(value)) {
+        return { '@__type': 'u32', '@__count': value.length, '#text': value.join(' ') };
+    }
+    else {
+        return { '@__type': 'u32', '#text': Number(value) };
+    }
+};
